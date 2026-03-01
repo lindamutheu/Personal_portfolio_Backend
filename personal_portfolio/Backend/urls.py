6984ec1eba7d7ce0d52b60563994646d17e0dd88
linda mutheu <lindamutheu5@gmail.com>
2026-03-01 19:28:54 +0300

    uncommitted changes

--- personal_portfolio/Backend/urls.py
+++ personal_portfolio/Backend/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import home
 
-
 from .views import (
     ProfileView,
     ProjectListView,
--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0!s_234!f-d6khlbx+3&3ll8!hl89wyzvot%xq&=#%jss(059#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -131,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
