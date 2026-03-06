a700daeb7a3a692e119e724870109547c941cb94
linda mutheu <lindamutheu5@gmail.com>
2026-03-06 22:36:55 +0300

    Add executable build script to root

--- personal_portfolio/build.sh
+++ personal_portfolio/build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+# 1. Install dependencies
 pip install -r requirements.txt
 
+# 2. Collect static files
 python manage.py collectstatic --no-input
+
+# 3. Run database migrations
 python manage.py migrate
